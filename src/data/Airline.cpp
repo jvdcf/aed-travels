009fade9dc809fd60d1533855374e6ef680e1966
Guilherme Matos <up202208755@up.pt>
2023-12-21 21:24:09 +0000

    <insert npc commit message> airline.cpp

--- src/data/Airline.cpp
+++ src/data/Airline.cpp
@@ -8,6 +8,12 @@ Airline::Airline(const std::string& code, const std::string& name, const std::st
     this->alias = alias;
     this->country = country;
 }
+Airline::Airline() {
+    this->code = 0;
+    this->name = nullptr;
+    this->alias = nullptr;
+    this->country = nullptr;
+}
 
 std::string Airline::getCodeStr() const {
     return codeToString(this->code);
@@ -29,7 +35,7 @@ std::string Airline::getCountry() const {
     return this->country;
 }
 
-uint16_t Airline::codeToHash(const std::string& code) const {
+uint16_t Airline::codeToHash(const std::string& code) {
     uint16_t res = 0;
     for (int i = 2; i >= 0; --i) {
         res <<= 5;
@@ -38,7 +44,7 @@ uint16_t Airline::codeToHash(const std::string& code) const {
     return res;
 }
     
-std::string Airline::codeToString(uint16_t code) const {
+std::string Airline::codeToString(uint16_t code) {
     std::string res;
     for (int i = 0; i < 3; ++i) { 
         res.push_back((code & (32-1)) + 'A'); // "& (32-1)": obtain the 5 Least Significant Bits. (32-1) = 31 = 0b11111
