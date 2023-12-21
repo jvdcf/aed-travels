bd7721a11ca6900fbed08ce4c194e58d1ce2a6f8
Guilherme Matos <up202208755@up.pt>
2023-12-21 18:22:39 +0000

    populating airport

--- src/data/Airport.cpp
+++ src/data/Airport.cpp
@@ -1,51 +1,61 @@
 #include "Airport.hpp"
+#include "Data.hpp"
+#include <cstdint>
 
 Airport::Airport(const std::string& code, const std::string& name, const std::string& city, const std::string& country,
                  float latitude, float longitude) {
-    // TODO
+    this->code      = codeToHash(code);
+    this->name      = name;
+    this->city      = city;
+    this->country   = country;
+    this->latitude  = latitude;
+    this->longitude = longitude;
+
 }
 
-std::string Airport::getCode() const {
-    // TODO
-    return std::string();
+std::string Airport::getCodeStr() const {
+    return codeToString(this->code);
+}
+
+uint16_t Airport::getCode() const {
+    return code;
 }
 
 std::string Airport::getName() const {
-    // TODO
-    return std::string();
+    return name;
 }
 
 std::string Airport::getCity() const {
-    // TODO
-    return std::string();
+    return city;
 }
 
 std::string Airport::getCountry() const {
-    // TODO
-    return std::string();
+    return country;
 }
 
 float Airport::getLatitude() const {
-    // TODO
-    return 0;
+    return latitude;
 }
 
 float Airport::getLongitude() const {
-    // TODO
-    return 0;
+    return longitude;
 }
 
-float Airport::DistanceTo(const Airport& airport) const {
-    // TODO
-    return 0;
+uint16_t Airport::codeToHash(const std::string& code) const {
+    uint16_t res = 0;
+    for (int i = 2; i >= 0; --i) {
+        res <<= 5;
+        res += (code[i] - 'A') & (32-1); // "& (32-1)": obtain the 5 Least Significant Bits. (32-1) = 31 = 0b11111
+    }
+    return res;
 }
-
-uint16_t Airport::codeToBytes(const std::string &code) const {
-    // TODO
-    return 0;
-}
-
+    
 std::string Airport::codeToString(uint16_t code) const {
-    // TODO
-    return std::string();
+    std::string res;
+    for (int i = 0; i < 3; ++i) { 
+        res.push_back((code & (32-1)) + 'A'); // "& (32-1)": obtain the 5 Least Significant Bits. (32-1) = 31 = 0b11111
+        code >>= 5;
+    }
+    return res;
 }
+
