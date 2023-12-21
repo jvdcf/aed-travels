8389b70040ad5e67076643d512e43a165b0001ff
Guilherme Matos <up202208755@up.pt>
2023-12-21 20:09:22 +0000

    changes to data

--- src/data/Data.cpp
+++ src/data/Data.cpp
@@ -1,17 +1,29 @@
 #include "Data.hpp"
+#include "Airline.hpp"
+#include "Airport.hpp"
+#include <cstdint>
 
 Data::Data() {
     // TODO
 }
 
 void Data::loadAirport(Airport &airport) {
-    // TODO
+    flights.addVertex(airport);
+    searchAirportByCode[airport.getCode()] = flights.findVertex(airport);
+    searchAirportByName[airport.getName()] = flights.findVertex(airport);
 }
 
 void Data::loadAirline(Airline &airline) {
-    // TODO
+    searchAirlines[airline.getCode()] = airline;
 }
 
-void Data::loadFlight(Airport &source, Airport &target, Airline &airline) {
-    // TODO
+void Data::loadFlight(uint16_t source_code, uint16_t dest_code, uint16_t airline_code) {
+    Vertex<Airport,Airline>* src = searchAirportByCode[source_code];
+    Vertex<Airport,Airline>* dst = searchAirportByCode[dest_code];
+    Airline air = searchAirlines[airline_code];
+    // TODO: distance
+    float distance = 0;
+    auto tmp = src->getAdj();
+    tmp.push_back(Edge<Airport,Airline>(dst, distance, air));
+    src->setAdj(tmp);
 }
