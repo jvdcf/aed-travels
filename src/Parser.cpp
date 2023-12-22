8773eb898c230e222b0a354cad7e9815730a3e0e
Guilherme Matos <up202208755@up.pt>
2023-12-22 16:24:04 +0000

    adding warnings and potentially fixing segfault

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -4,6 +4,8 @@
 #include "data/Data.hpp"
 #include <algorithm>
 #include <fstream>
+#include <iostream>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -33,6 +35,11 @@ void Parser::parseAirports(Data &data, const std::string& csv) {
     std::getline(s,line);
     while (std::getline(s,line)) {
         auto csv_fields = split_csv(line);
+        if (csv_fields.size() != 6) {
+            std::cerr << "WARNING: Invalid line in file that stores the airports' information." << std::endl;
+            std::cerr << "INFO    : If you only see one warning per file, you might have a trailing empty line." << std::endl;
+            continue;
+        }
         Airport entry = Airport(csv_fields[0], csv_fields[1], csv_fields[2], csv_fields[3], std::stof(csv_fields[4]), std::stof(csv_fields[5]));
         line.clear();
         data.loadAirport(entry);
@@ -45,6 +52,11 @@ void Parser::parseAirlines(Data &data, const std::string& csv) {
     std::getline(s,line);
     while (std::getline(s,line)) {
         auto csv_fields = split_csv(line);
+        if (csv_fields.size() != 4) {
+            std::cerr << "WARNING: Invalid line in file that stores the airlines' information." << std::endl;
+            std::cerr << "INFO    : If you only see one warning per file, you might have a trailing empty line." << std::endl;
+            continue;
+        }
         Airline entry = Airline(csv_fields[0], csv_fields[1], csv_fields[2], csv_fields[3]);
         line.clear();
         data.loadAirline(entry);
@@ -57,6 +69,11 @@ void Parser::parseFlights(Data &data, const std::string& csv) {
     std::getline(s,line);
     while (std::getline(s,line)) {
         auto csv_fields = split_csv(line);
+        if (csv_fields.size() != 3) {
+            std::cerr << "WARNING : Invalid line in file that stores the flights' information." << std::endl;
+            std::cerr << "INFO    : If you only see one warning per file, you might have a trailing empty line." << std::endl;
+            continue;
+        }
         line.clear();
         data.loadFlight(Airport::codeToHash(csv_fields[0]), Airport::codeToHash(csv_fields[1]), Airline::codeToHash(csv_fields[2]));
     }
